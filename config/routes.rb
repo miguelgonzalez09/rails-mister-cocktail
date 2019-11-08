afa2a51c872cc6ff8435a9cef49897d54689f335
Miguel Gonzalez <miguelgonzalez09.mg@gmail.com>
2019-11-08 12:33:58 -0600

    Update website

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:show, :index, :create, :new] do
     resources :doses, only: [:create, :new]
